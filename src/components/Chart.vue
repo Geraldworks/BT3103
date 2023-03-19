584ddc772da84067fafae3cd99898d5551a45358
Gerald Ho <97504394+Geraldworks@users.noreply.github.com>
2023-03-20 01:14:42 +0800

    added more styling and code housekeeping

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,29 +1,24 @@
 <template>
   <div>
-    <h1>{{ chartName }} - {{ recentData }}%</h1>
-    <line-chart class="lineChart" :data="chartData" :colors="chartColor"/>
+    <div>{{ chartName }} - {{ recentData }}{{ metric }}</div>
+    <line-chart class="lineChart" :data="chartData" :colors="chartColor" />
   </div>
 </template>
 
 <script>
 export default {
-  // this segment is to test the graphs
-  data() {
-    return {
-      // chartData: {
-      //   "Monday" : 1,
-      //   "Tuesday": 2,
-      //   "Wednesday": 3,
-      //   "Thursday": 4,
-      //   "Friday": 5
-      // }
-    }
-  },
   props: {
     chartName: String,
     chartData: Object,
     recentData: Number,
-    chartColor: Array
+    chartColor: Array,
+    metric: String
   },
 };
 </script>
+
+<style scoped>
+div {
+  font-size: 34px;
+}
+</style>
