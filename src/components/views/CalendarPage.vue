7d7e7a172341b87006c7e300f8707c41dd06cbb4
James Mak <jamesmakjx@gmail.com>
2023-03-22 02:30:34 +0800

    Added calendars

--- src/components/views/CalendarPage.vue
+++ src/components/views/CalendarPage.vue
@@ -1,10 +1,29 @@
 <template>
-<Navbar />
-<h1> This is calendar page</h1>
+    <Navbar />
+    <div style="height: 600px; width: 80%; margin: auto; padding: 1%;text-align: center;">
+        <vue-cal :time="false" active-view="month" :disable-views="['years', 'year', 'week']" />
+    </div>
 </template>
 
 <script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
 export default {
-    name: "CalendarPage"
-}
+  name: "CalendarPage",
+  components: { VueCal }
+};
 </script>
+
+<style>
+.vuecal__menu, .vuecal__cell-events-count {background-color: #ED1F24;}
+.vuecal__title-bar {background-color: #ff8082;}
+.vuecal__cell--today, .vuecal__cell--current {background-color: #ffd6d7;}
+.vuecal:not(.vuecal--day-view) .vuecal__cell--selected {background-color: #ff8082;}
+.vuecal__cell--selected:before {border-color: black;}
+/* Cells and buttons get highlighted when an event is dragged over it. */
+.vuecal__cell--highlighted:not(.vuecal__cell--has-splits),
+.vuecal__cell-split--highlighted {background-color: rgba(195, 255, 225, 0.5);}
+.vuecal__arrow.vuecal__arrow--highlighted,
+.vuecal__view-btn.vuecal__view-btn--highlighted {background-color: rgba(136, 236, 191, 0.25);}
+</style>
