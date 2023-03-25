2d15497ad8bb824963e21b4e618cbaae1d64fa24
Luther Tan <76609402+luthertan98@users.noreply.github.com>
2023-03-26 01:32:59 +0800

    done with backend stuff but frontend need improve

--- src/components/trainer/TrainerPerformanceHeader.vue
+++ src/components/trainer/TrainerPerformanceHeader.vue
@@ -1,44 +1,62 @@
 <template>
   <div class="profile-container">
     <!--Profile Picture-->
-    <img
-      class="profile-picture"
-      src="@/assets/images/test.jpeg"
-      alt="profile picture"
-    />
+    <img class="profile-picture" src="@/assets/images/test.jpeg" alt="profile picture" />
     <div class="your-profile">
       <div class="trainer-options">
         <div>
           Viewing <span style="color: #ed1f24">{{ clientName }}</span>
         </div>
+        <div class="right-side-items" @click="">Meal plan</div>
+        <div class="right-side-items" @click="">Routine</div>
+        <div class="right-side-items" @click=" () => { xlgDemo = true;}">Update Stats</div>
+        <CModal size="xl" :visible="xlgDemo" class="testing" @close=" () => {xlgDemo = false;}">
+          <UpdateForm :clientEmail="email" :clientName ="clientName" class="testing"/>
+        </CModal>
         <!-- If we detect that the trainer wants to return, we will emit "refresh" to 
-           go back to the page with all the client cards-->
-        <div class="right-side-items" @click="returnBackToTrainerHomePage()">
-          Return
-        </div>
+          go back to the page with all the client cards-->
+        <div class="right-side-items" @click="returnBackToTrainerHomePage()">Return</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { doc, collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebase.js";
+import { CModal, CFormInput, CFormFeedback, CFormLabel, CFormText } from "@coreui/vue";
+import UpdateForm from "./UpdateForm.vue";
 
 export default {
   name: "TrainerPerformanceHeader",
+  components: {
+    UpdateForm
+  },
   data() {
     return {
       clientName: null,
+      xlgDemo: false,
     };
   },
   props: {
-    email: String,
+    email:"",
   },
   methods: {
     returnBackToTrainerHomePage() {
       this.$emit("returnToHome");
     },
+    async updateDataBase() {
+      try {
+        const clientData = doc(db,'client', this.email);      
+        const myDoc = await getDocs(clientData);
+        const fatsData = myDoc.data();
+        console.log(fatsData);
+    } catch (error) {
+        console.log(error);
+        console.log("No email observed in database");
+      }
+
+    }
   },
   emits: ["returnToHome"],
   async created() {
@@ -109,7 +127,9 @@ export default {
   animation-duration: 0.3s;
   animation-fill-mode: forwards;
 }
-
+.testing {
+  background-color: black;
+}
 @keyframes turn-red {
   from {
     color: white;
