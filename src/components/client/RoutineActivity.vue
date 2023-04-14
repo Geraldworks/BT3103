dc606967af26fb69a50bc880e00376358f558d6e
bryankwe <e0725536@u.nus.edu>
2023-04-14 15:47:01 +0800

    Test Change to src reference
    Attempt to fix deployment error

--- src/components/client/RoutineActivity.vue
+++ src/components/client/RoutineActivity.vue
@@ -90,8 +90,8 @@ export default {
       row.insertCell(2).innerHTML = set.reps;
       // Set tick if true, else cross
       row.insertCell(3).innerHTML = set.done
-        ? '<img src="src/assets/images/tick-icon.png" style="width: 20px; margin-bottom: 5px;"/>'
-        : '<img src="src/assets/images/cross-black-icon.png" style="width: 20px; margin-bottom: 5px;"/>';
+        ? '<img src="@/assets/images/tick-icon.png" style="width: 20px; margin-bottom: 5px;"/>'
+        : '<img src="@/assets/images/cross-black-icon.png" style="width: 20px; margin-bottom: 5px;"/>';
       index++;
     });
   },
